cdba2e589e515b707eb7b22b2503ff8dff944a75
yvonnelalkovic <102817562+yvonnelalkovic@users.noreply.github.com>
2022-04-01 13:07:30 -0400

    Update listallthethings.sh

--- listallthethings.sh
+++ listallthethings.sh
@@ -8,8 +8,8 @@ do
 
     if [ $find_file ]     # if the file or directory exists
     then
-        echo "Output of ls -l $file:"
-        ls -l "$find_file"
+        echo "Output of ls -i $file:"
+        ls -i "$find_file"
 
     else                  # if the file or directory does not exist
         echo "$file not found, sorry!!"
